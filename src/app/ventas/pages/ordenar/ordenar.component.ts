e46a735660150715a2fd731c41a6b34ef74fbb3d
SebastianC8 <scorrales@garantiascomunitarias.com>
2021-07-21 12:56:54 -0500

    .

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -17,7 +17,7 @@ export class OrdenarComponent {
     { nombre: 'Batman', vuela: false, color: Color.negro },
     { nombre: 'Robin', vuela: false, color: Color.verde },
     { nombre: 'Daredvil', vuela: false, color: Color.rojo },
-    { nombre: 'Linterna verde', vuela: true, color: Color.verde },
+    { nombre: 'Linterna verde', vuela: true, color: Color.verde }
   ];
 
   cambiarMayuscula() {
